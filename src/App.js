39bb05974758e620396421719892caa10438b256
davidmgudeman <davidmgudeman@gmail.com>
2018-01-15 11:02:01 -0800

    building the code to enter the lottery

--- src/App.js
+++ src/App.js
@@ -21,6 +21,16 @@ class App extends Component {
 
     this.setState({ manager, players, balance });
   }
+
+  onSubmit = async event => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether'),
+    });
+  };
+
   render() {
     return (
       <div>
@@ -31,7 +41,7 @@ class App extends Component {
           {web3.utils.fromWei(this.state.balance, 'ether')} ether!
         </p>
         <hr />
-        <form>
+        <form onSubmit={this.onSubmit}>
           <h4>Want to try your luck?</h4>
           <div>
             <label>Amount of ether to enter</label>
