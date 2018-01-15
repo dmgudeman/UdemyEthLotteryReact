67c505367acafa33d4a3dae6e100fd5f53075c35
davidmgudeman <davidmgudeman@gmail.com>
2018-01-15 10:43:44 -0800

    started making form on the UI

--- src/App.js
+++ src/App.js
@@ -5,21 +5,43 @@ import web3 from './web3';
 import lottery from './lottery';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { manager: '' };
-  }
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+    value: '',
+  };
 
   async componentDidMount() {
     // the provider from metamask already has a default account there. So no arguments need to be in the call()
     const manager = await lottery.methods.manager().call();
-    this.setState({ manager });
+    const players = await lottery.methods.getPlayers().call();
+    // want to get the balance of the lottery contract.  Use the options.address to obtain this with web3
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
   }
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is manged by {this.state.manager}</p>
+        <p>
+          This contract is manged by {this.state.manager}. There are currently{' '}
+          {this.state.players.length} people entered, competing to win{' '}
+          {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+        </p>
+        <hr />
+        <form>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
       </div>
     );
   }
